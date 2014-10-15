d7e899febd1a946e030f9e03c8c13756fe6c0d81
brnikita <nikita-bragin@mail.ru>
2014-10-16 00:59:49 +0400

    update telescope application

---
+++ packages/telescope-theme-grid/lib/client/grid.js
@@ -0,0 +1,2 @@
+templates["posts_list"] = 'grid_posts_list';
+templates["grid_post_submit"] = 'grid_post_submit';
---
+++ packages/telescope-theme-grid/package.js
@@ -0,0 +1,18 @@
+Package.describe({
+    summary: 'Telescope Grid theme',
+    version: '0.1.0',
+    name: 'telescope-theme-grid'
+});
+
+Package.on_use(function (api) {
+
+    api.use(['templating', 'telescope-base'], ['client']);
+
+    api.add_files([
+        'lib/client/css/grid_screen.css',
+        'lib/client/templates/grid_posts_list.html',
+        'lib/client/templates/grid_post_submit.html',
+        'lib/client/grid.js'
+    ], ['client']);
+
+});
